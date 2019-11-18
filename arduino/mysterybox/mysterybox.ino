a69771aa771dded362caea059d26cf68b1f3fe19
Luca Ebner <ebnerl@student.ethz.ch>
2019-11-18 01:17:09 +0100

    Created separate library for sevendots stage. mysterybox.ino now uses this library instead of containing the code.

--- arduino/mysterybox/mysterybox.ino
+++ arduino/mysterybox/mysterybox.ino
@@ -5,15 +5,16 @@
   Date: November 2019
 */
 
+#include "sevendots.h"
+
+
 int stage = 0;
 const bool debug = false;
 
 // 7dots
 const int led[] = {0,1,2,3,4,5,6}; //LED pins
 const int button[] = {7,8,9,10,11,12,13}; //TODO remap these pin numbers, arduino uno doesnt have enough pins
-bool buttonstate[] = {0,0,0,0,0,0,0};
-bool ledstate[] = {0,0,0,0,0,0,0};
-bool reset[] = {0,0,0,0,0,0,0};
+SevenDots sevenDots(led, button);
 
 //MagnetPanel
 const int magnetPanelPin = A0;
@@ -23,61 +24,9 @@ const int metalBarPin = A1;
 
 
 bool stage1(){
-
-  if((ledstate[0]==true) && (ledstate[1]==true) && (ledstate[2]==true) && (ledstate[3]==true) && (ledstate[4]==true) && (ledstate[5]==true) && (ledstate[6]==true)){
-    if(debug){Serial.println("[7dots]: All LEDs turned on. Stage one complete.");}
-  
-    //visual confirmation
-    for(int j=0; j<4; j++){
-      for(int i=0; i<7; i++){
-         digitalWrite(led[i], LOW);
-      }
-      delay(500);
-      for(int i=0; i<7; i++){
-         digitalWrite(led[i], HIGH);
-      }
-      delay(500);
-    }
-      for(int i = 0; i<7; i++){
-         ledstate[i] = reset[i];
-         digitalWrite(led[i], LOW);
-      }
-
-      stage++;
+  if(sevenDots.spinOnce()){
+    if(debug){Serial.println("[stage1]: All 7 LEDs on, success! Proceed with stage 2.");}
   }
-
-  //update button readings
-  for(int i = 0; i<7; i++){
-    buttonstate[i] = digitalRead(button[i]);
-  }
-
-  for(int i = 0; i<7; i++){
-    if (buttonstate[i] == HIGH){
-
-      //Serial.println(button[i]);
-      
-      digitalWrite(i, !ledstate[i]);
-      ledstate[i] = !ledstate[i];
-      if(i==0){
-        digitalWrite(6, !ledstate[6]);
-        digitalWrite(1, !ledstate[1]);
-        ledstate[6] = !ledstate[6];
-        ledstate[1] = !ledstate[1];
-      } else if(i==6){
-        digitalWrite(5, !ledstate[5]);
-        digitalWrite(0, !ledstate[0]);
-        ledstate[5] = !ledstate[5];
-        ledstate[0] = !ledstate[0];
-      } else{ //usual case, neighbor leds should switch as well
-        digitalWrite(i-1, !ledstate[i-1]);
-        digitalWrite(i+1, !ledstate[i+1]);
-        ledstate[i-1] = !ledstate[i-1];
-        ledstate[i+1] = !ledstate[i+1];
-      }
-        delay(500);
-    }
-  }
-
 }
 
 
@@ -86,17 +35,14 @@ bool stage2(){
     stage++;
     if(debug){Serial.println("[stage2]: Magnet Panel combination correct! Proceed with stage 3.");}
   }
-
 }
 
 
 bool stage3(){
-
   if(digitalRead(metalBarPin) == HIGH){
     stage++;
     if(debug){Serial.println("[stage3]: Metal Bar connectors bridged, success! Proceed with stage 4.");}
   }
-  
 }
 
 
@@ -106,10 +52,7 @@ void setup() {
   if(debug){Serial.begin(9600);}
 
   // 7dots
-  for(int i = 0; i<7; i++){
-    pinMode(led[i], OUTPUT);
-    pinMode(button[i], INPUT);
-  }
+  sevenDots.setup();
 
   //MagnetPanel
   pinMode(magnetPanelPin, INPUT);
@@ -126,7 +69,10 @@ void loop() {
   switch(stage){
 
     case 1: //7dots
-      stage1();
+      if(stage1()){
+        stage++;
+        //TODO: maybe redeclare pinModes here? this allows re-using pins
+      }
       break;
 
     case 2: //MagnetPanel
