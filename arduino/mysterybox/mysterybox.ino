65bce8dde870a2d27b1cecec5617f98d9bf3aa3e
Luca Ebner <ebnerl@student.ethz.ch>
2019-11-18 15:25:30 +0100

    mysterybox.ino now uses metalbar library instead of containing the code.

--- arduino/mysterybox/mysterybox.ino
+++ arduino/mysterybox/mysterybox.ino
@@ -7,9 +7,10 @@
 
 #include "sevendots.h"
 #include "magnetpanel.h"
+#include "metalbar.h"
 
 /// config ///
-const bool debug = false;
+const bool debug = true;
 
 
 // 7dots
@@ -24,6 +25,7 @@ MagnetPanel magnetPanel(magnetPanelPin);
 
 // MetalBar
 const int metalBarPin = A1;
+MetalBar metalBar(metalBarPin);
 
 int stage = 0;
 
@@ -31,38 +33,46 @@ int stage = 0;
 bool stage1(){
   if(sevenDots.spinOnce()){
     if(debug){Serial.println("[stage1]: All 7 LEDs are on, success!");}
+    return true;
   }
+  return false;
 }
 
 
 bool stage2(){
   if(magnetPanel.spinOnce()){
     if(debug){Serial.println("[stage2]: Magnet Panel combination correct!");}
+    return true;
   }
+  return false;
 }
 
 
 bool stage3(){
   if(digitalRead(metalBarPin) == HIGH){
-    stage++;
-    if(debug){Serial.println("[stage3]: Metal Bar connectors bridged, success! Proceed with stage 4.");}
+    if(debug){Serial.println("[stage3]: Metal Bar connectors bridged, success!");}
+    return true;
   }
+  return false;
 }
 
 
 void setup() {
   // put your setup code here, to run once:
 
-  if(debug){Serial.begin(9600);}
+  if(debug){
+    Serial.begin(9600);
+    delay(2000);
+  }
 
   // 7dots
-  sevenDots.setup();
+  //sevenDots.setup();
 
   //MagnetPanel
   magnetPanel.setup();
 
   //MetalBar
-  pinMode(metalBarPin, INPUT);
+  metalBar.setup();
     
   stage = 1;
   if(debug){Serial.println("[Setup]: Initialized.");}
@@ -74,7 +84,7 @@ void loop() {
 
     case 1: //7dots
       if(stage1()){
-        if(debug){Serial.println("[switch]: Stage 1 complete. Proceed with stage 2.");}
+        if(debug){Serial.println("[Switch]: Stage 1 complete. Proceed with stage 2.");}
         stage++;
         //TODO: maybe redeclare pinModes here? this allows re-using pins
       }
@@ -82,20 +92,24 @@ void loop() {
 
     case 2: //MagnetPanel
       if(stage2()){
-        if(debug){Serial.println("[switch]: Stage 2 complete. Proceed with stage 3.");}
+        if(debug){Serial.println("[Switch]: Stage 2 complete. Proceed with stage 3.");}
         stage++;
       }
       break;
 
     case 3:
-      stage3();
+      if(stage3()){
+        if(debug){Serial.println("[Switch]: Stage 3 complete. Proceed with stage 4.");}
+        stage++;
+      }
       break;
 
     default:
       if(debug){
-        Serial.print("[switch]: Error! default case, stage: ");
+        Serial.print("[Switch]: Error! default case, stage: ");
         Serial.println(stage);
       }
+      delay(500);
       break;
 
   }
@@ -110,6 +124,4 @@ void loop() {
     }
   }
   
-
-
 }
